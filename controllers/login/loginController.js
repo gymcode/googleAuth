1804a296d67956d09f9430014adfdd29c1142484
Kenneth <kyleabs20@gmail.com>
2020-11-22 21:22:43 -0800

    dealing the login section

--- controllers/login/loginController.js
+++ controllers/login/loginController.js
@@ -7,18 +7,32 @@ const getlogin = async(req, res)=>{
 }
 
 const login = async(req, res)=>{
-    const {email, password} = req.body
+    const {email, password} = req.body;
+    console.log(email)
+    console.log(password)
 
     //validation check
-    const {error, message} = loginCheck(...req.body)
-    if (error) {
-        return res.status(406).send({
-            status: "error", 
-            errorMessage: message
-        })
-    }
+    // const {error, message} = loginCheck(email, password)
+    // console.log(error)
+    // if (error) res.status(406).send({status: "error",errorMessage: message})
+
+    // checking is the user is already in the database
+    const checkUser = await User.findOne({email})
+    if (!checkUser) res.status(403).send({status: "error", errorMessage: "User does not exist int he database" })
+
+    //comparing the user password
+    const passwordCompare = await bcrypt.compare(password, checkUser.password)
+    if(!passwordCompare) res.status(400).send({status: "error", errorMessage: "password does not match up"})
+
+    res.status(200).json({
+        status: "success", 
+        message: "your are logged in bro", 
+        data: checkUser
+    })
+
 }
 
 module.exports = {
-    getlogin
+    getlogin,
+    login
 }
--- routes/web.js
+++ routes/web.js
@@ -1,10 +1,11 @@
 const route = require('express').Router()
 
 const {getData, registerUser} = require('../controllers/register/registerController.js'); 
-const {getlogin} = require('../controllers/login/loginController')
+const {getlogin, login} = require('../controllers/login/loginController')
 
 route.get('/register', getData)
 route.post('/register', registerUser)
 route.get('/login', getlogin)
+route.post('/login', login)
 
 module.exports = route
