e3f1856bce14a71659dfb8d1c55f57a161ee8ca5
Kenneth <kyleabs20@gmail.com>
2020-11-22 20:34:27 -0800

    dealing the login section

--- controllers/login/loginController.js
+++ controllers/login/loginController.js
@@ -1,5 +1,6 @@
 const User = require('../../database/userModel'); 
 const bcrypt = require('bcryptjs')
+const {loginCheck} = require('../../validation/validator');
 
 const getlogin = async(req, res)=>{
     res.send("move on bro")
@@ -7,7 +8,15 @@ const getlogin = async(req, res)=>{
 
 const login = async(req, res)=>{
     const {email, password} = req.body
-    
+
+    //validation check
+    const {error, message} = loginCheck(...req.body)
+    if (error) {
+        return res.status(406).send({
+            status: "error", 
+            errorMessage: message
+        })
+    }
 }
 
 module.exports = {
--- validation/validator.js
+++ validation/validator.js
@@ -1,4 +1,4 @@
-const validationCheck = ({firstname, lastname, username, password}, res) => {
+const validationCheck = ({firstname, lastname, username, password}) => {
     if (!firstname.trim() || !lastname.trim() || !username.trim() || !password.trim()) 
       return {
           error: true, 
@@ -16,6 +16,24 @@ const validationCheck = ({firstname, lastname, username, password}, res) => {
 
 }
 
+const loginCheck = ({email, password})=>{
+    if (!email.trim() || !password.trim()) 
+        return {
+            error: true, 
+            message: "all fields are required and must be filled up before proceeding"
+        }
+    if (email < 4 || password < 4) {
+        return {
+            error: true, 
+            message: "individual fields must not have characters less than 4"
+        }
+    }
+    return {
+        error:false
+    }
+} 
+
 module.exports = {
-    validationCheck
+    validationCheck, 
+    loginCheck
 }
